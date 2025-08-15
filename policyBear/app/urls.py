e774c0495ec9aa3565a7cf2de79b26abe86d5cb9
Monirul Islam Akash <aaj7731@gmail.com>
2025-08-15 19:41:29 +0600

    PB Upload to Aws

--- policyBear/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app.views import *
-
-urlpatterns = [
-    path('', index, name='home'),
-    path('affordable-health-insurance', affordable_health_insurance, name='affordable-health-insurance'),
-    path('supplement-insurance', supplement_insurance, name='supplement-insurance'),
-    path('api/ai/agent/', api_for_ai_agent, name='api_for_ai_agent'),
-]
